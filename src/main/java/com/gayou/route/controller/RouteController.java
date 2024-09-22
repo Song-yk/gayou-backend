5d6bd67352254c2aee728386e2e86d3ca0abcefe
Cho_c <ghktmsgkrh@naver.com>
2024-09-23 05:03:09 +0900

    feat: kakao api login

--- src/main/java/com/gayou/route/controller/RouteController.java
+++ src/main/java/com/gayou/route/controller/RouteController.java
@@ -1,6 +1,5 @@
 package com.gayou.route.controller;
 
-import com.gayou.auth.dto.UserDto;
 import com.gayou.route.dto.RouteHeadDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,7 +8,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import com.gayou.route.service.RouteService;
 
-import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -19,6 +17,13 @@ public class RouteController {
     @Autowired
     private RouteService routeService;
 
+    /**
+     * 사용자의 경로를 저장하는 메서드
+     *
+     * @param username - 현재 인증된 사용자의 사용자 이름 (JWT 토큰에서 추출된 값)
+     * @param routeDTO - 저장할 경로 정보 (RouteHeadDto)
+     * @return ResponseEntity<Long> - 생성된 경로의 ID와 함께 HTTP 상태 코드를 반환
+     */
     @PostMapping("/locations")
     public ResponseEntity<Long> saveCourse(@AuthenticationPrincipal String username,
             @RequestBody RouteHeadDto routeDTO) {
@@ -27,6 +32,12 @@ public class RouteController {
         return new ResponseEntity<>(routeHeadId, HttpStatus.CREATED);
     }
 
+    /**
+     * 사용자가 저장한 경로 목록을 반환하는 메서드
+     *
+     * @param username - 현재 인증된 사용자의 사용자 이름 (JWT 토큰에서 추출된 값)
+     * @return ResponseEntity<List<RouteHeadDto>> - 사용자가 저장한 경로 목록을 반환
+     */
     @GetMapping("/locations")
     public ResponseEntity<?> getMyCourse(@AuthenticationPrincipal String username) {
         List<RouteHeadDto> data = routeService.getMyRoute(username);
