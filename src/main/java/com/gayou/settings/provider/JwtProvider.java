74f0fe7b3837160af16b28e9e4de37051a1cf368
Cho_c <ghktmsgkrh@naver.com>
2024-09-24 06:21:18 +0900

    fix: remove jwt expire

--- src/main/java/com/gayou/settings/provider/JwtProvider.java
+++ src/main/java/com/gayou/settings/provider/JwtProvider.java
@@ -1,7 +1,5 @@
 package com.gayou.settings.provider;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -18,13 +16,11 @@ public class JwtProvider {
     private String SECRET_KEY;
 
     public String create(String email) {
-        Date expiredDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
 
         String jwt = Jwts.builder()
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .setSubject(email)
                 .setIssuedAt(new Date())
-                .setExpiration(expiredDate)
                 .compact();
 
         return jwt;
